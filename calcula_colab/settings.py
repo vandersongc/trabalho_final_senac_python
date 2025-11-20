f77e505605b63a2e58d934726c63dc5c2a160618
VANDERSON GOUVEIA DA COSTA <vanderson.costa@aluno.senac.rio>
2025-11-19 21:25:49 -0300

    v5

--- calcula_colab/settings.py
+++ calcula_colab/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!g6%_ls_-r_i=e#yr^hd9)eqtl#y72d8ru&4dm=az2u1sh*)r7'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-!g6%_ls_-r_i=e#yr^hd9)eqtl#y72d8ru&4dm=az2u1sh*)r7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
