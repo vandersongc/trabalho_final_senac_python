a83c671508847d825a028936993ae5fb156b1440
VANDERSON GOUVEIA DA COSTA <vanderson.costa@aluno.senac.rio>
2025-11-24 21:07:37 -0300

    perdi a versao ja

--- financeiro_rh/admin.py
+++ financeiro_rh/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import HistoricoCalculo
 
-# Register your models here.
+# Configuração para melhorar a visualização na lista do Admin
+class HistoricoCalculoAdmin(admin.ModelAdmin):
+    # Colunas que serão exibidas na tabela
+    list_display = ('usuario', 'tipo', 'salario_base', 'resultado_liquido', 'data_calculo')
+    
+    # Filtros laterais para ajudar na busca
+    list_filter = ('tipo', 'data_calculo')
+    
+    # Barra de pesquisa (busca pelo nome do usuário ou e-mail)
+    search_fields = ('usuario__username', 'usuario__email')
+    
+    # Ordenação padrão (do mais recente para o mais antigo)
+    ordering = ('-data_calculo',)
+
+# Registra o modelo com a configuração acima
+admin.site.register(HistoricoCalculo, HistoricoCalculoAdmin)
